63a20547407af3313c65ef95af9b1778745a8ed0
Adam <behrens.adam@gmail.com>
2023-07-06 10:18:01 -0400

    update pip description

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
 setup(
-    version='0.0.3',
+    version='0.0.4',
+    description='Python client library for the Tychos API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires='>=3.6',
     install_requires=[
         'requests',
