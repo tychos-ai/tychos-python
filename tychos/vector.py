e22dc450aa05b320a911d9ac3c80f722f1451b6b
Adam <behrens.adam@gmail.com>
2023-07-10 14:03:00 -0400

    update api_key and .gitignore

--- tychos/vector.py
+++ tychos/vector.py
@@ -4,28 +4,13 @@ from . import api_key
 
 class _Vector:
     def __init__(self):
-        if api_key is None:
-            raise ValueError("API key not set. Please set the API key using 'tychos.api_key = <your_api_key>'. If you need to create an API key, you can go so at tychos.ai")
         self.api_key = api_key
         self.base_url = 'https://www.tychos.ai/api/'
         # self.base_url = 'http://localhost:3000/api/'
-    
-    async def initialize(self):
-        await self._verify_api_key()
-
-    async def _verify_api_key(self):
-        unkey_client = unkey.Client(api_key=self.api_key)
-        await unkey_client.start()
-        result = await unkey_client.keys.verify_key(self.api_key)
-
-        if result.is_ok:
-            data = result.unwrap()
-        else:
-            raise ValueError(result.unwrap_err())
-        
-        await unkey_client.close()
         
     def create(self, type, input_text, model, model_provider_key=None):
+        if self.api_key is None:
+            raise ValueError("API key not set. Please set the API key using 'tychos.api_key = <your_api_key>'. If you need to create an API key, you can go so at tychos.ai")
         if type == "text_embedding":
             if model == "text-embedding-ada-002":
                 try:
--- tychos/vector_data_store.py
+++ tychos/vector_data_store.py
@@ -6,35 +6,14 @@ from . import api_key
 
 class VectorDataStore:
     def __init__(self):
-        if api_key is None:
-            raise ValueError("API key not set. Please set the API key using 'tychos.api_key = <your_api_key>'. If you need to create an API key, you can go so at tychos.ai")
         self.api_key = api_key
         self.base_url = 'https://www.tychos.ai/api/'
         # self.base_url = 'http://localhost:3000/api/'
-        self.start()
-
-    def start(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.initialize())
-    
-    async def initialize(self):
-        await self._verify_api_key()
         self.vector = _Vector()
-        await self.vector.initialize()
-    
-    async def _verify_api_key(self):
-        unkey_client = unkey.Client(api_key=self.api_key)
-        await unkey_client.start()
-        result = await unkey_client.keys.verify_key(self.api_key)
-
-        if result.is_ok:
-            data = result.unwrap()
-        else:
-            raise ValueError(result.unwrap_err())
-        
-        await unkey_client.close()
 
     def query(self, name, query_string, limit):
+        if self.api_key is None:
+            raise ValueError("API key not set. Please set the API key using 'tychos.api_key = <your_api_key>'. If you need to create an API key, you can go so at tychos.ai")
         # vectorize query string
         query_vector = self.vector.create(
             type="text_embedding",
@@ -58,6 +37,8 @@ class VectorDataStore:
         return response.json()
     
     def list(self):
+        if self.api_key is None:
+            raise ValueError("API key not set. Please set the API key using 'tychos.api_key = <your_api_key>'. If you need to create an API key, you can go so at tychos.ai")
         url = f'{self.base_url}/datasets'
         headers = {'api_key': self.api_key}
         response = requests.get(url=url, headers=headers)
