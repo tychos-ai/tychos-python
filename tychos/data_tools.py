93a2418d9a2b1b50350db9fe4b4b30a2b07ddfc0
Adam <behrens.adam@gmail.com>
2023-09-27 22:40:32 -0700

    refactor transform logic

--- tychos/data_tools.py
+++ tychos/data_tools.py
@@ -34,61 +34,31 @@ class DataTools:
         if not issubclass(target_schema, pydantic.BaseModel):
             raise TypeError("target_schema must be a subclass of pydantic.BaseModel")
         schema_name = target_schema.__name__
-        schema = DataTools.create_openai_properties(target_schema)
-        messages = [{"role": "user", "content": f"Use the supplied functions to parse the following text. Do not make up answers. If you can't find a value for a property, simply return None for that property: {data}"}]
-            
+        schema = target_schema.model_json_schema()
+        
+        messages = [
+            {"role": "user", 
+            "content": f"Use the supplied functions to parse the following text. Do not make up answers. If you can't find a value for a property, simply return None for that property: {data}"
+            }
+        ]
         functions = [
             {
                 "name": schema_name,
                 "description": target_schema.__doc__ or "Extracts data from unstructured text into a json schema that matches the output_cls. For missing values or data not found, do not hallucinate and only complete with 'None'",
-                "parameters": {
-                    "type": "object",
-                    "properties": schema.get('properties'),
-                    "required": schema.get('required')
-                },
+                "parameters": schema,
             }
         ]
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            functions=functions,
-            function_call={"name": schema_name},
-        )
-        
-        if not response.get("choices"):
-            raise ValueError("OpenAI's response does not contain the expected 'choices' field.")
-        
-        response_message = response["choices"][0]["message"]
-        
-        if not isinstance(response_message, dict):
-            raise TypeError(f"Expected 'response_message' to be a dictionary, but got {type(response_message)}")
-            
-        if "function_call" not in response_message:
-            raise KeyError("The response_message does not contain a 'function_call'.")
-
-        if "function_call" not in response_message:
-            raise KeyError("The response_message does not contain a 'function_call'.")
-
-        if response_message["function_call"]["name"] != schema_name:
-            raise ValueError(f"Function '{response_message['function_call']['name']}' is not recognized or supported.")
         
         try:
-            function_args = json.loads(response_message["function_call"]["arguments"])
-        except json.JSONDecodeError as e:
-            raise ValueError(f"Error decoding the JSON arguments: {e}")
-        
-        try:
-            result = target_schema(**function_args)
-        except Exception as e:
-            raise ValueError(f"Error creating output class: {e}")
-        
-        function_response = result.model_dump()
-        messages.append(response_message)
-        messages.append(
-            {
-                "role": "function",
-                "name": schema_name,
-                "content": json.dumps(function_response),
-            }
-        )
-        return messages[2]["content"]
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                functions=functions,
+                function_call={"name": schema_name},
+            )
+
+            function_args = response["choices"][0]["message"]["function_call"]["arguments"]
+            result = target_schema(**json.loads(function_args))
+            return result
+        except (KeyError, pydantic.ValidationError, json.JSONDecodeError):
+            return {"error": "Failed to transform data"}
